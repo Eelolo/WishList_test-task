3b2c17b2a0be136d06b133519470c3dba647d11e
Eelolo <Eelolo@yandex.ru>
2021-03-06 18:20:52 +0300

    added buttons

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import sys
 import os
 from dotenv import load_dotenv
 from PyQt5.QtWidgets import (
-    QWidget, QApplication, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QListWidget
+    QWidget, QApplication, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QListWidget,
+    QPushButton
 )
 from db import Database
 
@@ -67,10 +68,36 @@ class WishList(QWidget):
         wishes.addWidget(self.wish_list)
         container.addLayout(wishes)
 
+        buttons = QHBoxLayout()
+        self.create_btn = QPushButton()
+        self.create_btn.setText('Create')
+        self.create_btn.clicked.connect(self.create_wish)
+        self.save_btn = QPushButton()
+        self.save_btn.setText('Save')
+        self.save_btn.clicked.connect(self.update_wish)
+        self.delete_btn = QPushButton()
+        self.delete_btn.setText('Delete')
+        self.delete_btn.clicked.connect(self.delete_wish)
+
+        buttons.addWidget(self.create_btn)
+        buttons.addWidget(self.save_btn)
+        buttons.addWidget(self.delete_btn)
+
+        common_container.addLayout(buttons)
         self.setLayout(common_container)
 
     def selection_changed(self, item):
         pass
+
+    def create_wish(self):
+        pass
+
+    def delete_wish(self):
+        pass
+
+    def update_wish(self):
+        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
