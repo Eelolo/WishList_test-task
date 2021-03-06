208cca904944ab59fb46a6b749cbf74105a869da
Eelolo <Eelolo@yandex.ru>
2021-03-06 17:30:14 +0300

    create_db function

--- db.py
+++ db.py
@@ -8,7 +8,11 @@ class Database:
         self.password = passwd
         self.database_name = db_name
         self.table_name = table_name
-        self.create_connection()
+        try:
+            self.create_connection()
+        except mysql.connector.errors.ProgrammingError:
+            self.create_db()
+            self.create_connection()
 
     def create_connection(self):
         db = mysql.connector.connect(
@@ -19,3 +23,29 @@ class Database:
         )
 
         self.db = db
+
+    def create_db(self):
+        db = mysql.connector.connect(
+            host=self.localhost,
+            user=self.username,
+            passwd=self.password,
+        )
+
+        self.db = db
+
+        cursor = self.db.cursor()
+
+        cursor.execute("DROP DATABASE IF EXISTS wishlist")
+        cursor.execute("CREATE DATABASE wishlist")
+
+        cursor.execute("USE wishlist")
+        cursor.execute('''
+            CREATE TABLE wishes(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name CHAR(50) NOT NULL UNIQUE,
+            price INT,
+            link CHAR(255),
+            note CHAR(255)
+        )''')
+
+        self.db.commit()
